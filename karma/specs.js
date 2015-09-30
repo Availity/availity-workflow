7511e6dc815f0101c408b69c16ee8f82ed684f5d
Rob McGuinness <rob.mcguinness@availity.com>
2015-09-30 11:59:12 -0400

    add instanbul instrumenter to Karma

--- karma/specs.js
+++ karma/specs.js
@@ -1,5 +1,13 @@
-require('angular');
-require('angular-mocks');
 
-var context = require.context((process.cwd() + '/project/app'), true, /-spec.js$/ );
-context.keys().forEach(context);
+module.parent.require('./project/app/vendor');
+module.parent.require('angular-mocks');
+
+// require all SPEC files
+var textContext = require.context((process.cwd() + '/project/app'), true, /-spec.js$/ );
+textContext.keys().forEach(textContext);
+
+// require all SOURCE files
+var sourceContext = require.context((process.cwd() + '/project/app'), true, /index\.js$/);
+sourceContext.keys().forEach(sourceContext);
+
+
