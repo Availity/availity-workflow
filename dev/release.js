a51752b9114c1cbbdf3f0ee1c0daf18c3d95b07b
Rob McGuinness <rob.mcguinness@availity.com>
2016-03-09 12:28:23 -0500

    move to npm

---
+++ dev/release.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const semver = require('semver');
+const inquirer = require('inquirer');
+
+let VERSION = null;
+let RAW = null;
+
+module.exports = function release() {
+
+};
