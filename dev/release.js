5ff352432e9eb8362bdc8ee29a57727e800cbd1e
Rob McGuinness <rob.mcguinness@availity.com>
2016-03-27 15:24:26 -0400

    prompt first then lint

--- dev/release.js
+++ dev/release.js
@@ -3,8 +3,8 @@ var lint = require('./lint');
 
 module.exports =  function release() {
 
-  return lint()
-   .then(version.prompt)
+  return version.prompt
+   .then(lint)
    .then(version.bump)
    .then(version.tag);
 
