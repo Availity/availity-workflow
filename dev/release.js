b9980d20404ec9804c29f429c798ebc1c5fc94df
Rob McGuinness <rob.mcguinness@availity.com>
2016-03-28 10:48:38 -0400

    add spinner to release task
    * cleanup webpack output so that only one stats info is emitted

--- dev/release.js
+++ dev/release.js
@@ -1,10 +1,19 @@
+var del = require('del');
+
+var context = require('../context');
 var version = require('./version');
 var lint = require('./lint');
+var copy = require('./copy');
+var build = require('./build');
 
 function release() {
 
+  del.sync([context.settings.dest()]);
+
   return version.prompt()
    .then(lint)
+   .then(copy)
+   .then(build)
    .then(version.bump)
    .then(version.tag);
 
--- logger/index.js
+++ logger/index.js
@@ -1,8 +1,16 @@
 var dateformat = require('dateformat');
+var figures = require('figures');
 
 var template = '[{grey:%s}]} {yellow:[av-workflow]} ';
 
 var Logger = require('eazy-logger').Logger;
+
+var proto = Logger.prototype;
+
+proto.ok = function(message) {
+  this.info('{green:%s} %s', figures.tick, message);
+};
+
 
 var logger = new Logger({
   prefix: template.replace('%s', dateformat(new Date(), 'HH:MM:ss')),
