0448df43e2c6011d50ab58b3cd66967803178c7e
Rob McGuinness <rob.mcguinness@availity.com>
2016-03-30 10:41:04 -0400

    release tasks for workflow

---
+++ tools.js
@@ -0,0 +1,47 @@
+var eslint = require('eslint');
+var globby = require('globby');
+var Promise = require('bluebird');
+var ora = require('ora');
+
+var logger = require('./logger');
+
+function lint() {
+
+  var engine = new eslint.CLIEngine({
+    useEslintrc: true
+  });
+
+  return new Promise(function(resolve, reject) {
+
+    logger.info('Started linting');
+    var spinner = ora('running linter rules');
+    spinner.color = 'yellow';
+    spinner.start();
+
+    globby(['**/*.js', '!node_modules/**']).then(function(paths) {
+
+      var report = engine.executeOnFiles(paths.slice(2));
+
+      if (report.errorCount || report.warningCount) {
+
+        spinner.stop();
+        var formatter = engine.getFormatter();
+        logger.info('' + formatter(report.results));
+        logger.error('Failed linting');
+        reject();
+
+      } else {
+
+        spinner.stop();
+        logger.ok('Completed linting ');
+        resolve();
+
+      }
+
+    });
+
+  });
+
+}
+
+lint();
