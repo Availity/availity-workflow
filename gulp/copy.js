60f1bdf84807c4e800854270481d0380d15aa7c5
Rob McGuinness <rob.mcguinness@availity.com>
2016-03-30 06:03:07 -0400

    wrap npm copy script within legacy gulp task

--- gulp/copy.js
+++ gulp/copy.js
@@ -1,9 +1,8 @@
-var logger = require('../logger');
 var context = require('../context');
+var copy = require('../dev/copy');
 
 context.gulp.task('av:copy', ['av:copy:templates']);
 
-context.gulp.task('av:copy:templates', function(cb) {
-  logger.warn('gulp task [av:copy:templates] is being deprecated in availity-workflow v2.');
-  cb();
+context.gulp.task('av:copy:templates', function() {
+  return copy();
 });
