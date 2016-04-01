dc7e8346a3935be6265508da771187f67ac73ee3
Rob McGuinness <rob.mcguinness@availity.com>
2016-04-01 10:50:03 -0400

    alias gulp build script to npm build script

--- gulp/build.js
+++ gulp/build.js
@@ -1,44 +1,9 @@
-var webpack = require('webpack');
-var gUtil = require('gulp-util');
-var _ = require('lodash');
-
 var context = require('../context');
 var logger = require('../logger');
+var build = require('../dev/build');
 
-
-context.gulp.task('av:build', function(cb) {
-
-  var webpackConfig = require('../webpack').get();
-
-  var compiler = webpack(webpackConfig);
-  var bundleCounts = Object.keys(webpackConfig.entry).length;
-
-  var done = _.after(bundleCounts, function() {
-    cb();
-  });
-
-  compiler.run(function(err, stats) {
-
-    if (err) {
-      throw new gUtil.PluginError('webpack', err);
-    }
-
-    var _stats = stats.toString({
-      colors: true,
-      cached: true,
-      reasons: false,
-      source: false,
-      chunks: false
-    });
-
-    gUtil.log('[av:build]', _stats);
-
-    // Webpack will emit stats per entry point bundle created.  Gulp will
-    // cry foul if the callback is called multiple times.
-    done();
-
-  });
-
+context.gulp.task('av:build', function() {
+  return build();
 });
 
 context.gulp.task('av:build:dev', function() {
