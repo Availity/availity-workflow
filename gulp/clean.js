39ba521cd0d35d37b1d7d93b244297aa697bfea2
Rob McGuinness <rob.mcguinness@availity.com>
2016-07-29 12:34:25 -0400

    adios gulp

--- gulp/clean.js
+++
@@ -1,6 +0,0 @@
-var context = require('../context');
-var del = require('del');
-
-context.gulp.task('av:clean', function() {
-  return del.sync([context.settings.dest()]);
-});
--- gulp/watch.js
+++
@@ -1,7 +0,0 @@
-var context = require('../context');
-
-context.gulp.task('av:watch', ['av:watch:templates']);
-
-context.gulp.task('av:watch:templates', function() {
-  return context.gulp.watch(context.settings.templates.src, ['av:copy:templates']);
-});
