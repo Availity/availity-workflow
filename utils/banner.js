021705458fc28077acbde7d6e6168d6ef1894f37
Rob McGuinness <rob.mcguinness@availity.com>
2016-07-29 13:11:27 -0400

    conver to es6

--- utils/banner.js
+++ utils/banner.js
@@ -1,19 +1,18 @@
-module.exports = function() {
+import fs from 'fs';
+import path from 'path';
+import config from '../config';
 
-  var fs = require('fs');
-  var path = require('path');
-  var utils = require('gulp-util');
-  var config = require('../config');
+export default banner() => {
 
-  return utils.template(
-    '/**\n' +
-    ' * <%= package.name %> v<%= package.version %> -- <%= today %>\n' +
-    ' */\n',
-    {
-      file: '',
-      package: JSON.parse(fs.readFileSync(path.join(config.project.path, 'package.json'))),
-      today: new Date().toISOString().substr(0, 10)
-    }
-  );
+  const  package = JSON.parse(fs.readFileSync(path.join(config.project.path, 'package.json')));
+  const today = new Date().toISOString().substr(0, 10)
 
-};
+  return `
+/**
+ * ${package.name} v${package.version} -- ${today}
+ *
+ *
+ /**`;
+
+}
+
