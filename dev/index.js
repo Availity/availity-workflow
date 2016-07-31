cd9056210075d38a3123b4e5d35fcdeca1e007af
Robert McGuinness <robert.mcguinness.iii@gmail.com>
2016-07-31 10:44:24 -0400

    remove complexity report

--- dev/index.js
+++ dev/index.js
@@ -8,7 +8,6 @@ var commands = {
   build: require('./build'),
   version: require('./version'),
   start: require('./start'),
-  complexity: require('./complexity'),
   test: require('./test').continous
 };
 
