778fb1d93e4f922b6b93d214cc474f8ce40eb765
Rob McGuinness <rob.mcguinness@availity.com>
2016-10-13 14:35:51 -0400

    use html extension for import and htm extension for template cache

--- webpack/loaders/angular.js
+++ webpack/loaders/angular.js
@@ -8,6 +8,14 @@ module.exports = [
   { test: /[\\\/]moment\.js$/, loader: 'expose?moment' },
   {
     test: /\.html$/,
+    loader: 'html',
+    // Ignore following templates else errors like:
+    //    - "window is not defined" error from the html-webpack-plugin
+    //    - "The path for file doesn't contains relativeTo param"  from ngtemplate-loader
+    exclude: /(index\.html|react-template\.html|angular-template\.html)/
+  },
+  {
+    test: /\.htm$/,
     loader: `ngtemplate?relativeTo=${process.cwd()}/!html`,
     // Ignore following templates else errors like:
     //    - "window is not defined" error from the html-webpack-plugin
