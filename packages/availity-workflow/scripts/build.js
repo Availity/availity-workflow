658823369aab05d38b7c6d02e94680f4c94edf6a
Rob McGuinness <rob.mcguinness@availity.com>
2017-02-09 04:56:53 -0500

    start refactoring build and release cli

---
+++ packages/availity-workflow/scripts/build.js
@@ -0,0 +1,60 @@
+const Promise = require('bluebird');
+const webpack = require('webpack');
+const ProgressPlugin = require('webpack/lib/ProgressPlugin');
+const ora = require('ora');
+const Logger = require('availity-workflow-logger');
+
+const plugin = require('.plugin');
+
+function bundle() {
+
+  return new Promise( (resolve, reject) => {
+
+
+    const webpackConfig = plugin('webpack.config.production');
+
+    Logger.info('Started compiling');
+    const spinner = ora('Running webpack');
+    spinner.color = 'yellow';
+    spinner.start();
+
+    webpackConfig.plugins.push(new ProgressPlugin( (percentage, msg) => {
+
+      const percent = percentage * 100;
+
+      if (percent % 20 === 0 && msg !== null && msg !== undefined && msg !== ''){
+        spinner.text = `Webpack ${msg}`;
+      }
+
+    }));
+
+    webpack(webpackConfig).run((err, stats) => {
+
+      spinner.stop();
+
+      if (err) {
+        Logger.failed('Failed compiling');
+        reject(err);
+        return;
+      }
+
+      const statistics = stats.toString({
+        colors: true,
+        cached: true,
+        reasons: false,
+        source: false,
+        chunks: false,
+        children: false
+      });
+
+      Logger.info(statistics);
+      Logger.ok('Finished compiling');
+      resolve();
+
+    });
+
+  });
+
+}
+
+module.exports = bundle;
---
+++ packages/availity-workflow/scripts/release.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const del = require('del');
+const settings = require('availity-workflow-settings');
+const Logger = require('availity-workflow-logger');
+
+const version = require('./version');
+const lint = require('./lint');
+const build = require('./build');
+
+
+function release() {
+
+  del.sync([settings.dest()]);
+
+  return version.prompt()
+    .then(() =>{
+      Logger.info('Started releasing');
+    })
+    .then(lint)
+    .then(version.bump)
+    .then(build)
+    .then(version.tag)
+    .then(() => {
+      Logger.ok('Finished releasing');
+    })
+    .catch(() => {
+      Logger.fail('Failed releasing');
+    });
+
+}
+
+module.exports = release;
+
