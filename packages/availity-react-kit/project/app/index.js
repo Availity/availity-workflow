e6be3e48ddbcd9b588c1e017920470adfef69218
Rob McGuinness <rob.mcguinness@availity.com>
2017-02-10 20:38:18 -0500

    more whitespace please

--- packages/availity-react-kit/project/app/index.js
+++ packages/availity-react-kit/project/app/index.js
@@ -12,14 +12,15 @@ ReactDOM.render(
 
 // Hot Module Replacement API
 if (module.hot) {
+
   module.hot.accept('./App', () => {
     const NextApp = require('./App').default;
     ReactDOM.render(
       <AppContainer>
         <NextApp />
-      </AppContainer>
-      ,
+      </AppContainer>,
       document.getElementById('root')
     );
   });
+
 }
