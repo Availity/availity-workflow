1a07933a4a1ab0216ca94ca3777f21ade86b291c
Rob McGuinness <rob.mcguinness@availity.com>
2017-02-10 20:37:59 -0500

    remove CardImg from example

--- packages/availity-react-kit/project/app/request/Request.js
+++ packages/availity-react-kit/project/app/request/Request.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBlock,
+import { Card, CardText, CardBlock,
   CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 const Request = () => {
   return (
     <div>
       <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
         <CardBlock>
           <CardTitle>Card title</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
