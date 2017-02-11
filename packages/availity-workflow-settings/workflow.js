6546ddb84c43eb26939770857835aeb9aaac8a34
Rob McGuinness <rob.mcguinness@availity.com>
2017-02-10 23:43:01 -0500

    add comments to workflow settings

--- packages/availity-workflow-settings/workflow.js
+++ packages/availity-workflow-settings/workflow.js
@@ -3,24 +3,47 @@ const path = require('path');
 module.exports = {
 
   development: {
+
     // Opens the url in the default browser
     open: '#',
-    // Build status system notifications
-    notifications: false,
+
+    // Webpack build status system notifications
+    notification: true,
+
+    // Webpack dev server host
     host: '127.0.0.1',
-    port: 3000
+
+    // Webpack dev server port
+    port: 3000,
+
+    // Enable hot reloading server. The behavior for hot reloading depend on
+    // the appropriate availity-workflow plugin like availity-workflow-react
+    // and availity-workflow-angular.
+    hot: true
+
   },
 
   ekko: {
-    host: '127.0.0.1',
+
+    // Sets default latency for all route responses
     latency: 250,
-    port: 9999,
+
+    // Folder that containers the mock data files
     data: path.join(process.cwd(), 'project/data'),
+
+    // Path to route configuration file used by Ekko to build Express routes
     routes: path.join(process.cwd(), 'project/config/routes.json'),
+
+    // Enables or disables Ekko
     enabled: true,
+
+    // Array of NPM module names that enhance Ekko with additional data and routes.
+    // @See https://github.com/Availity/availity-mock-data
     plugins: [
       'availity-mock-data'
     ],
+
+    // Mock data can be passed a context used in the HATEOS urls in a response
     pluginContext: 'http://localhost:3000/api'
   },
 
