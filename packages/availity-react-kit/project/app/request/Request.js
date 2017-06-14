0c169083574522161fa9ee621842d68e58bd5812
Evan Sharp <evan.sharp@availity.com>
2017-06-14 10:56:45 -0400

    Clean up and improve react kit

--- packages/availity-react-kit/project/app/request/Request.js
+++ packages/availity-react-kit/project/app/request/Request.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Card, CardText, CardBlock,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Button, Card, CardText, CardTitle, CardSubtitle, Container } from 'reactstrap';
 
 const Request = () => {
   return (
-    <div>
-      <Card>
-        <CardBlock>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBlock>
+    <Container className="mt-2">
+      <Card block>
+        <CardTitle>Card title</CardTitle>
+        <CardSubtitle>Card subtitle</CardSubtitle>
+        <CardText>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+          We rely heavily on <a href="https://reactstrap.github.io/" target="_blank">reactstrap</a> to avoid manually applying bootstrap/UIKit classes to elements.
+          <a href="https://reactstrap.github.io/components/" target="_blank">Check out all of the available components and their examples</a>
+        </CardText>
+        <div>
+          <Button color="primary">Button</Button>
+        </div>
       </Card>
-    </div>
+    </Container>
   );
 };
 
--- packages/availity-react-kit/project/app/request/Request.test.js
+++ packages/availity-react-kit/project/app/request/Request.test.js
@@ -1,5 +1,4 @@
 /* global it */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Request from './Request';
