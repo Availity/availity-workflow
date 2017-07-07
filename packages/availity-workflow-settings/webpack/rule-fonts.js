545a74b6234fe91397f179591e8cf70a2d45f383
Rob McGuinness <rob.mcguinness@availity.com>
2017-07-06 20:11:14 -0400

    update regex for font rule

--- packages/availity-workflow-settings/webpack/rule-fonts.js
+++ packages/availity-workflow-settings/webpack/rule-fonts.js
@@ -4,7 +4,7 @@ module.exports = {
   //  '../fonts/availity-font.eot?18704236'
   //  '../fonts/availity-font.eot'
   //
-  test: /-font\.(otf|ttf|woff2?|eot|svg)(\?.*)?$/,
+  test: /-font\.(otf|ttf|woff2?|eot|svg)(\?.+)?$/,
   use: [
     'file-loader?name=fonts/[name].[ext]'
   ]
