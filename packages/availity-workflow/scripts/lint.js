95a228f3f35e2cc58854db93e89892b1a9b9e5f2
Ben Beirut <ben.beirut@availity.com>
2017-08-21 22:18:53 -0400

    Ignore git untracked files when linting
    `npm run lint` now ignores files that are not tracked by git.

--- packages/availity-workflow/scripts/lint.js
+++ packages/availity-workflow/scripts/lint.js
@@ -5,6 +5,8 @@ const ora = require('ora');
 const chalk = require('chalk');
 const Logger = require('availity-workflow-logger');
 const settings = require('availity-workflow-settings');
+const path = require('path');
+const { spawnSync } = require('child_process');
 
 function lint() {
 
@@ -32,14 +34,29 @@ function lint() {
     spinner.color = 'yellow';
     spinner.start();
 
-    const files = settings.js();
+    // files tracked by git
+    let gitTrackedFiles;
+    const gitRootCmd = spawnSync('git', ['rev-parse', '--show-toplevel']);
+    if (gitRootCmd.status) {
+      // non-zero exit code; assume git repository absent
+      gitTrackedFiles = null;
+    } else {
+      const gitLsFiles = spawnSync('git', ['ls-files']);
+      gitTrackedFiles = gitLsFiles.stdout.toString().trim().split('\n');
+      const gitRoot = gitRootCmd.stdout.toString().trim();
+      gitTrackedFiles = gitTrackedFiles.map((f) => path.join(gitRoot, f));
+    }
 
     // Uses globby which defaults to process.cwd() and path.resolve(options.cwd, "/")
-    globby(files).then(paths => {
+    globby(settings.js()).then(paths => {
 
       spinner.stop();
+      const filesToLint = gitTrackedFiles ?
+        // git repository present
+        paths.filter((f) => ~gitTrackedFiles.indexOf(f)) :
+        paths;
 
-      const report = engine.executeOnFiles(paths);
+      const report = engine.executeOnFiles(filesToLint);
 
       if (report.errorCount || report.warningCount) {
 
