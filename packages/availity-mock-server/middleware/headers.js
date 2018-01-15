4df043f30c9a4cca6c02b00f111f78b243f537bd
Rob McGuinness <rob.mcguinness@availity.com>
2018-01-15 11:05:41 -0500

    refactor(availity-mock-server): use Availity Mock Server for x-powered-by header

--- packages/availity-mock-server/middleware/headers.js
+++ packages/availity-mock-server/middleware/headers.js
@@ -3,7 +3,7 @@ const config = require('../config');
 module.exports = function headers() {
   config.app.disable('x-powered-by');
   config.app.use((req, res, next) => {
-    res.setHeader('X-Powered-By', 'Ekko');
+    res.setHeader('X-Powered-By', 'Availity Mock Server');
     next();
   });
 };
