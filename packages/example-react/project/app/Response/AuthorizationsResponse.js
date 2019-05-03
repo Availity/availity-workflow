9f3b5e482e8049bd6a95112ea49b49fda3e62039
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-03 05:28:09 -0400

    Seperation component concern out of index.js.

---
+++ packages/example-react/project/app/Response/AuthorizationsResponse.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { observer, inject } from 'mobx-react';
+import { Card, CardBody } from 'reactstrap';
+import { Footer, Header } from '../components';
+import { Patient, Status, Transaction } from './components';
+import propTypes from './props';
+
+const AuthorizationsResponse = () => (
+  <div className="container-sm">
+    <Header />
+    <Transaction />
+    <Card>
+      <CardBody>
+        <Patient />
+        <Status />
+      </CardBody>
+    </Card>
+    <Footer />
+  </div>
+);
+
+AuthorizationsResponse.propTypes = propTypes;
+
+export default inject('stateStore')(observer(AuthorizationsResponse));
