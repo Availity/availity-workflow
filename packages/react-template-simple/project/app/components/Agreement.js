8f0089b9dfbdf13576ca42bc31707019a19927a0
Kyle Gray <Kyle.Gray@availity.com>
2019-06-05 10:06:20 -0400

    refactor(react-template-simple): added typography package

--- packages/react-template-simple/project/app/components/Agreement.js
+++ packages/react-template-simple/project/app/components/Agreement.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { AvFeedback, AvInput, AvGroup } from 'availity-reactstrap-validation';
+import { Disclaimer } from '@availity/typography';
 import { Label } from 'reactstrap';
 
 const Agreement = () => (
   <fieldset>
-    <div className="disclaimer">
+    <Disclaimer>
       I have fully read this agreement and understand that I am entering into a legally binding agreement and that my
       organization is bound by the terms and conditions contained therein. I attest and certify that I am the Primary
       Controlling Authority for the organization named herein and that I possess the necessary legal authority to bind
       this organization. I further attest and certify my organization&quote;s designation as a Covered Entity under{" "}
       <abbr title="Health Insurance Portability and Accountability Act">HIPAA</abbr>
-    </div>
+    </Disclaimer>
 
     <AvGroup check className="custom-control custom-checkbox">
       <AvInput
