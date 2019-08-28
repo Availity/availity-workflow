85113e97ea7babebcfd02d8a7bac20878bd30ad5
Kyle Gray <kgray1497@gmail.com>
2019-08-28 15:36:37 -0400

    feat(docs): init dosc

---
+++ packages/docs/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  pathPrefix: '/docs/availity-workflow',
+  __experimentalThemes: [
+    {
+      resolve: '@availity/gatsby-theme',
+      options: {
+        root: __dirname,
+        subtitle: 'Availity Workflow',
+        description: 'Documentation for Availity Workflow',
+        githubRepo: 'availity/availity-workflow',
+        sidebarCategories: {
+          null: ['index', 'getting-started','upgrade'],
+          Plugins: ['plugins/react', 'plugins/angular'],
+          Mocking: ['mock/server', 'mock/data']
+        }
+      }
+    }
+  ]
+};
