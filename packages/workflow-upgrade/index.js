a7464ccf68bfff7f9b97ec9d870a7425ae83080a
Christopher Havekost <Christopher.Havekost@availity.com>
2019-11-20 12:32:56 -0500

    feat(workflow-upgrade): add latest versions of eslint-config-availity and @availity/workflow, refactor order of commands

--- packages/workflow-upgrade/index.js
+++ packages/workflow-upgrade/index.js
@@ -6,7 +6,7 @@ const { exec } = require('child_process');
 const Logger = require('@availity/workflow-logger');
 
 module.exports = cwd => {
-  Logger.info('Upgrading @availity/workflow to v7.0.0');
+  Logger.info('Upgrading @availity/workflow');
   const pkgFile = path.join(cwd, 'package.json');
 
   if (fs.existsSync(pkgFile)) {
@@ -35,15 +35,10 @@ module.exports = cwd => {
       delete devDependencies['@availity/workflow-plugin-angular'];
     }
 
-    Logger.info('Upgrading dependencies');
-    // Set the new version of the workflow
-    devDependencies['eslint-config-availity'] = '^5.2.0';
-    devDependencies['@availity/workflow'] = '^7.0.0';
-
     // Update package.json
     fs.writeFileSync(pkgFile, `${JSON.stringify(pkg, null, 2)}\n`, 'utf-8');
 
-    let installCommand = '';
+    let installer = '';
     const pkgLock = path.join(cwd, 'package-lock.json');
     const yarnLock = path.join(cwd, 'yarn.lock');
 
@@ -51,21 +46,34 @@ module.exports = cwd => {
       // delete package lock, set npm install command
       Logger.info('Deleting Package-Lock');
       fs.unlinkSync(pkgLock);
-      installCommand = 'npm install';
+      installer = 'npm';
     } else if (fs.existsSync(yarnLock)) {
       // delete yarn lock, set yarn install command
       Logger.info('Deleting yarn.lock');
       fs.unlinkSync(yarnLock);
-      installCommand = 'yarn install';
+      installer = 'yarn';
     }
 
     Logger.info('Deleting node modules...');
     // Delete Node Modules
     rimraf.sync(path.join(cwd, 'node_modules'));
 
-    Logger.info('Re-Installing node modules..');
+    Logger.info('Adding latest versions of eslint-config-availity and @availity/workflow');
+
+    if (installer === 'yarn') {
+      exec(`${installer} add eslint-config-availity @availity/workflow --dev`, () => {
+        Logger.success('\nCongratulations! Welcome to the new @availity/workflow.');
+      });
+    } else if (installer === 'npm') {
+      // installer -i packages --save-dev
+      exec(`${installer} install eslint-config-availity @availity/workflow --save-dev`, () => {
+        Logger.success('\n');
+      });
+    }
+
+    Logger.info('Reinstalling node modules..');
     // Run install command
-    exec(installCommand, () => {
+    exec(`${installer} install`, () => {
       Logger.success('\nCongratulations! Welcome to the new @availity/workflow.');
     });
   }
