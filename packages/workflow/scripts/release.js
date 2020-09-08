01c4bdba4ffcd517591a3987e40d94124ecaf075
Rob McGuinness <rob.mcguinness@availity.com>
2020-09-08 19:00:16 -0400

    refactor(workflow): try and print error stack trace

--- packages/workflow/scripts/release.js
+++ packages/workflow/scripts/release.js
@@ -15,8 +15,7 @@ async function release({ settings }) {
     await version.tag();
     Logger.success('Finished releasing');
   } catch (error) {
-    Logger.failed(`Failed releasing - fix errors and retry.
-`);
+    Logger.failed(`Failed releasing ${error.message}\n\n ${error.stack}`);
     throw error;
   }
 }
