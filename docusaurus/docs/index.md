f2a1d71eaa60b645711efb04e30028ff8926f8dc
Brandon Robinson <Brandon.Robinson@availity.com>
2020-10-30 09:34:03 -0400

    feat(docs): add docusaurus docs

---
+++ docusaurus/docs/index.md
@@ -0,0 +1,32 @@
+---
+title: Getting Started
+---
+
+Toolkit for Availity web projects.
+
+Workflow is a toolkit designed for kick-starting React web projects on the Availity Portal.
+
+## Get Started
+
+There are 2 main ways to get started with Workflow:
+
+1. [Tutorial:](/tutorial/) Step-by-Step instructions on how to install and start a project. This also includes details on the project structure as well as what each file/folder is used for.
+2. [Quick Start:](/quick-start/) The TLDR guide to kickstarting a project from start to finish.
+
+## Navigating The Framework
+
+Our Open-Source Framework is comprised of 3 major/core building blocks:
+
+### [Workflow](/)
+
+Our toolkit for building web applications. Similar to [CRA](https://reactjs.org/docs/create-a-new-react-app.html)
+
+### [Components](https://availity.github.io/availity-react)
+
+React Component library built using Availity UIKit and Bootstrap 4
+
+### [Resources](https://availity.github.io/sdk-js)
+
+Mono-repo of Javascript packages for helping build out custom components.
+
+If you can't find what you are looking for on any of the left sub menus try out the `search bar` at the top of every page that leverages [Algolia](https://www.algolia.com/) to provide lightning fast searches across all of our docs.
---
+++ docusaurus/docusaurus.config.js
@@ -0,0 +1,118 @@
+module.exports = {
+  plugins: [],
+  themes: [['@docusaurus/theme-search-algolia', { id: '01' }]],
+  onBrokenLinks: 'log',
+  title: 'Availity Docs',
+  tagline: 'React components using Availity UIKit and Bootstrap 4',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'availity', // Usually your GitHub org/user name.
+  projectName: 'availity-react', // Usually your repo name.
+  themeConfig: {
+    algolia: {
+      apiKey: 'eec0154a008662c32d440b7de7982cd2',
+      indexName: 'availity'
+    },
+    announcementBar: {
+      id: 'supportus',
+      backgroundColor: '#e29f0d',
+      textColor: 'black',
+      content:
+        '⭐️ If you like Availity-React, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/availity/availity-react">GitHub</a>! ⭐️'
+    },
+    colorMode: { defaultMode: 'dark' },
+
+    navbar: {
+      title: 'Availity Docs',
+      hideOnScroll: true,
+      logo: {
+        alt: 'Availity Docs Logo',
+        src: 'img/icon.png'
+      },
+      items: [
+        // {
+        //   type: 'doc',
+        //   docId: 'index',
+        //   label: 'Getting Started',
+        //   position: 'left'
+        // },
+        // {
+        //   type: 'doc',
+        //   docId: 'intro',
+        //   label: 'Components',
+        //   position: 'left'
+        // },
+        // {
+        //   type: 'doc',
+        //   docId: 'quickstart',
+        //   label: 'SDK',
+        //   position: 'left'
+        // },
+        // {
+        //   type: 'doc',
+        //   docId: 'intro',
+        //   label: 'Cli',
+        //   position: 'left'
+        // },
+        // {
+        //   to: 'docs/contributing',
+        //   label: 'SDK',
+        //   position: 'right'
+        // },
+        // {
+        //   to: 'docs/contributing',
+        //   label: 'Workflow',
+        //   position: 'right'
+        // },
+        // {
+        //   to: '/',
+        //   activeBasePath: 'docs',
+        //   label: 'Docs',
+        //   position: 'left',
+        // },
+        {
+          to: 'http://localhost:3000', // needs to be updated once deployed
+          label: 'Availity-Workflow',
+          position: 'right'
+        },
+        {
+          to: 'http://localhost:3001', // needs to be updated once deployed
+          label: 'SDK',
+          position: 'right'
+        },
+        {
+          to: 'https://deploy-preview-571--condescending-leavitt-66e607.netlify.app/', // The target URL (string).
+          label: 'Components',
+          position: 'right'
+        },
+
+        {
+          href: 'https://github.com/availity',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository'
+        }
+      ]
+    }
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          routeBasePath: '/',
+          homePageId: 'index',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/availity/availity-react/edit/feat/docusaurus-docs/docusaurus/'
+        },
+
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      }
+    ]
+  ]
+};
