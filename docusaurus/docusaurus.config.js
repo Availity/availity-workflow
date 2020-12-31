43d36a9dce05113e7eb90d3ddd5541ce3704b44d
Christopher Havekost <Christopher.Havekost@availity.com>
2020-12-31 11:49:58 -0500

    fix(dinosaurdocs): fixes broken editUrl

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -75,7 +75,7 @@ module.exports = {
 
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/availity/availity-workflow/edit/master/docusaurus/docs/'
+          editUrl: 'https://github.com/availity/availity-workflow/edit/master/docusaurus/'
         },
 
         theme: {
