db9e1a40a6624c7e970497aededf5e6bc3fbd6a5
Christopher Havekost <Christopher.Havekost@availity.com>
2021-10-28 22:49:23 -0400

    build: removes version plugin due to conflict with monodeploy
    Adds config changes for monodeploy

--- monodeploy.config.js
+++ monodeploy.config.js
@@ -1,13 +1,19 @@
 // CLI flags will take precedence over these default settings
 module.exports = {
+  access: 'infer',
+  autoCommit: false,
+  changelogFilename: './CHANGELOG.md',
+  // changesetFilename: 'needed?',
+  changesetIgnorePatterns: ['**/*.test.js', '**/*.spec.{js,ts}', '**/*.{md,mdx}', '**/yarn.lock'],
+  conventionalChangelogConfig: 'conventional-changelog-conventionalcommits',
   dryRun: false,
   git: {
     commitSha: 'HEAD',
     remote: 'origin',
     push: true
   },
-  conventionalChangelogConfig: '@tophat/conventional-changelog-config',
-  access: 'infer',
-  persistVersions: false,
-  changesetIgnorePatterns: ['**/*.test.js', '**/*.spec.{js,ts}', '**/*.{md,mdx}', '**/yarn.lock']
+  persistVersions: false, // TODO: turn on with autoCommit?
+  prerelease: false,
+  prereleaseId: 'alpha',
+  prereleaseNPMTag: 'canary'
 };
