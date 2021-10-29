67fd45f0c689b4869bdcf3343a0488245522842d
Christopher Havekost <Christopher.Havekost@availity.com>
2021-10-29 13:21:04 -0400

    build: updates release config

--- monodeploy.config.js
+++ monodeploy.config.js
@@ -1,18 +1,17 @@
 // CLI flags will take precedence over these default settings
+// https://tophat.github.io/monodeploy/configuration
 module.exports = {
   access: 'infer',
-  autoCommit: false,
+  autoCommit: true,
   changelogFilename: 'CHANGELOG.md',
   // changesetFilename: 'needed?',
   changesetIgnorePatterns: ['**/*.test.js', '**/*.spec.{js,ts}', '**/*.{md,mdx}', '**/yarn.lock'],
   conventionalChangelogConfig: 'conventional-changelog-conventionalcommits',
   dryRun: false,
   git: {
-    commitSha: 'HEAD',
-    remote: 'origin',
     push: true
   },
-  persistVersions: false, // TODO: turn on with autoCommit?
+  persistVersions: true,
   prerelease: false,
   prereleaseId: 'alpha',
   prereleaseNPMTag: 'canary'
