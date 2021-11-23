8b66b8a5aa60fed84564e00ffad1b973104b9790
Christopher Havekost <Christopher.Havekost@availity.com>
2021-11-22 22:20:12 -0500

    chore(workflow): updates babel-jest import to resolve breaking changes in v27

--- packages/workflow/jest/babel.js
+++ packages/workflow/jest/babel.js
@@ -1,5 +1,5 @@
 // https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/config/jest/babelTransform.js
-const babelJest = require('babel-jest');
+const babelJest = require('babel-jest').default;
 const babelPreset = require('../babel-preset');
 
 const createTransformer = () =>
