e9d17e5f1f176dbf2abda2b624dada95a4c0d36d
Christopher Havekost <Christopher.Havekost@availity.com>
2021-12-15 20:53:34 -0500

    chore(example): updates log levels

--- example/project/config/workflow.js
+++ example/project/config/workflow.js
@@ -2,12 +2,21 @@ const path = require('path');
 
 // TODO: add more complex workflow features for building/testing
 module.exports = (config) => {
-  config.development.open = '/';
+  // config.development.open = '/';
 
   config.development.hotLoader = true;
 
+  config.development.stats = {
+    level: 'normal',
+  };
+
+  config.development.infrastructureLogging = {
+    level: 'info',
+  };
+
   config.ekko.enabled = false;
 
+  // specify how to resolve node_modules since yarn hoists them to top-level
   config.modifyWebpackConfig = (webpackConfig, settings) => {
     webpackConfig.resolveLoader.modules.push(path.join(settings.project(), '../node_modules'));
   };
