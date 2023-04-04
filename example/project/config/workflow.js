990dc4832c3f9944b7e46b2a8f82dbe552990400
Benjamin Newman <benjamin.newman@availity.com>
2023-04-04 11:54:02 -0400

    feat(example): update example workflow config to use rspack safely

--- example/project/config/workflow.js
+++ example/project/config/workflow.js
@@ -20,85 +20,87 @@ module.exports = (config) => {
 
   // specify how to resolve node_modules since yarn hoists them to top-level
   config.modifyWebpackConfig = (webpackConfig, settings) => {
-    webpackConfig.resolveLoader.modules.push(path.join(settings.project(), '../node_modules'));
-    webpackConfig.resolve.plugins = [new TsConfigPathsPlugin()];
-    if (settings.isProduction() || settings.isDryRun()) {
-      // delete babel-loader rules
-      webpackConfig.module.rules[0].oneOf.splice(1, 2);
-      // use unshift to add rules, since they need to be placed _before_ the catch-all file loader
-      webpackConfig.module.rules[0].oneOf.unshift(
-        {
-          test: /\.tsx?$/,
-          include: settings.include(),
-          use: [
-            {
-              loader: 'esbuild-loader',
-              options: {
-                loader: 'tsx',
-                target: 'es2015',
+    if (!settings.__UNSAFE_EXPERIMENTAL_USE_RSPACK_DEV()) {
+      webpackConfig.resolveLoader.modules.push(path.join(settings.project(), '../node_modules'));
+      webpackConfig.resolve.plugins = [new TsConfigPathsPlugin()];
+      if (settings.isProduction() || settings.isDryRun()) {
+        // delete babel-loader rules
+        webpackConfig.module.rules[0].oneOf.splice(1, 2);
+        // use unshift to add rules, since they need to be placed _before_ the catch-all file loader
+        webpackConfig.module.rules[0].oneOf.unshift(
+          {
+            test: /\.tsx?$/,
+            include: settings.include(),
+            use: [
+              {
+                loader: 'esbuild-loader',
+                options: {
+                  loader: 'tsx',
+                  target: 'es2015',
+                },
               },
-            },
-          ],
-        },
-        {
-          test: /\.js|jsx|mjs?$/,
-          include: settings.include(),
-          exclude: [new RegExp(`node_modules[/\\\\](?=(@av/av-joi)).*`)],
-          use: [
-            {
-              loader: 'esbuild-loader',
-              options: {
-                loader: 'jsx',
-                target: 'es2015',
-              },
-            },
-          ],
-        },
-        // Process any JS outside of the app with esbuild.
-        // Unlike the application JS, we only compile the standard ES features.
-        {
-          test: /\.(js|mjs)$/,
-          exclude: settings.include(),
-          loader: 'esbuild-loader',
-          options: {
-            loader: 'js',
-            target: 'es2015',
+            ],
           },
-        }
-      );
-    } else {
-      webpackConfig.module.rules.splice(1, 1);
-      // add esbuild-loader for ts and tsx files
-      webpackConfig.module.rules.push(
-        {
-          test: /\.tsx?$/,
-          loader: 'esbuild-loader',
-          options: {
-            loader: 'tsx',
-            target: 'es2015',
-          },
-        },
-        {
-          test: /\.js|jsx|mjs?$/,
-          include: settings.include(),
-          exclude: [new RegExp(`node_modules[/\\\\](?=(@av/av-joi)).*`)],
-          use: [
-            {
-              loader: 'esbuild-loader',
-              options: {
-                loader: 'jsx',
-                target: 'es2015',
+          {
+            test: /\.js|jsx|mjs?$/,
+            include: settings.include(),
+            exclude: [new RegExp(`node_modules[/\\\\](?=(@av/av-joi)).*`)],
+            use: [
+              {
+                loader: 'esbuild-loader',
+                options: {
+                  loader: 'jsx',
+                  target: 'es2015',
+                },
               },
+            ],
+          },
+          // Process any JS outside of the app with esbuild.
+          // Unlike the application JS, we only compile the standard ES features.
+          {
+            test: /\.(js|mjs)$/,
+            exclude: settings.include(),
+            loader: 'esbuild-loader',
+            options: {
+              loader: 'js',
+              target: 'es2015',
             },
-          ],
-        }
+          }
+        );
+      } else {
+        webpackConfig.module.rules.splice(1, 1);
+        // add esbuild-loader for ts and tsx files
+        webpackConfig.module.rules.push(
+          {
+            test: /\.tsx?$/,
+            loader: 'esbuild-loader',
+            options: {
+              loader: 'tsx',
+              target: 'es2015',
+            },
+          },
+          {
+            test: /\.js|jsx|mjs?$/,
+            include: settings.include(),
+            exclude: [new RegExp(`node_modules[/\\\\](?=(@av/av-joi)).*`)],
+            use: [
+              {
+                loader: 'esbuild-loader',
+                options: {
+                  loader: 'jsx',
+                  target: 'es2015',
+                },
+              },
+            ],
+          }
+        );
+      }
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+        })
       );
     }
-    webpackConfig.plugins.push(
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-      })
-    );
     webpackConfig.resolve.fallback = Object.assign(webpackConfig.resolve.fallback, { url: false, buffer: false });
   };
 
