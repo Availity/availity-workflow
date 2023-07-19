139a90c36c06f6f48305e0bc517e0f482064db12
L. Roxanne young <Roxanne.Young@availity.com>
2023-07-19 09:13:03 -0400

    refactor(example): add json for testing

--- example/project/app/components/Form.tsx
+++ example/project/app/components/Form.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import { Field } from '@availity/form';
 import { Form as RForm, CardHeader, CardBody, CardFooter, Button } from 'reactstrap';
+import config from './form-config.json';
 
 const Form: React.SFC<FormikProps<FormValues>> = ({ handleSubmit, handleReset }) => (
   <RForm onSubmit={handleSubmit}>
     <CardHeader className="text-center h4 lead" tag="h3">
-      Test Card Header
+      {config.header}
     </CardHeader>
     <CardBody>
-      <Field name="formField" label="Some Field Label" />
-      <Field name="chainedField" label="Another Field Label" />
-      <Field name="nullishCoalescedField" label="Yet Another Field Label" />
+      {config.fields.map((fieldProps) => (
+        <Field key={fieldProps.name} {...fieldProps} />
+      ))}
     </CardBody>
     <CardFooter className="d-flex justify-content-end">
       <Button color="secondary" onClick={handleReset}>
