29303b6fefa7f52435e7325261c8154560704b6b
Greg Martin <greg.martin@availity.com>
2024-03-25 16:59:41 -0400

    fix(mock-server): lint issues

--- packages/mock-server/response/tests/behavior.spec.js
+++ packages/mock-server/response/tests/behavior.spec.js
@@ -10,16 +10,16 @@ describe('Behavior', () => {
   it('should respond with 404 for undefined route', async () => {
     try {
       await request.get(helper.getUrl('/dummy/route'));
-    } catch (err) {
-      expect(err.status).toBe(404);
+    } catch (error) {
+      expect(error.status).toBe(404);
     }
   });
 
   it('should respond with 404 when file does not exist', async () => {
     try {
       await request.get(helper.getUrl('/bad/file'));
-    } catch (err) {
-      expect(err.status).toBe(404);
+    } catch (error) {
+      expect(error.status).toBe(404);
     }
   });
 });
