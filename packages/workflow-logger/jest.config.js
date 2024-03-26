14c6abcdc19cfd674b311df5df84a805d32d0ff1
Greg Martin <greg.martin@availity.com>
2024-03-26 15:16:17 -0400

    test: run tests via @nx/jest

---
+++ packages/workflow-logger/jest.config.js
@@ -0,0 +1,11 @@
+const config = {
+  coverageDirectory: '../../coverage/workflow-logger',
+  displayName: 'workflow-logger',
+  globals: {},
+  cache: false,
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).js'],
+  transform: {},
+};
+
+export default config;
