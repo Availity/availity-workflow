5a51bb26de0b7e01b74174faaf6e23c9ea535445
Jordan Young <jordan.young@availity.com>
2026-06-16 17:59:31 -0400

    fix: update commitlint parser to support breaking change indicator (!)

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,4 +3,10 @@ export default {
   rules: {
     'header-max-length': [0, 'always', 85],
   },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w*)(?:\((.*)\))?(!)?:\s(.*)$/,
+      headerCorrespondence: ['type', 'scope', 'breaking', 'subject'],
+    },
+  },
 };
